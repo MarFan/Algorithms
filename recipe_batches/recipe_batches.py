d5b61f12d02ac129d12f320a055dbd7d16361a95
Mr. Jonah <jaitch@gmail.com>
2020-04-16 11:28:59 -0500

    Start baking!

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,20 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+	tots_batch = None
 
+	for key in recipe:
+		if key not in ingredients:
+			tots_batch = 0 # missing an ingredient, can make the dish
+			return tots_batch	
+		elif key in ingredients and tots_batch == None or ingredients[key] // recipe[key] < tots_batch:
+				tots_batch = ingredients[key] // recipe[key]
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+	return tots_batch
+
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
