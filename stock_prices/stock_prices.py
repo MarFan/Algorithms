286a64a27220701af0041e3eb9140605b6ecf8c8
Mr. Jonah <jaitch@gmail.com>
2020-04-16 09:44:05 -0500

    Reflecting

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -8,13 +8,19 @@ def find_max_profit(prices):
 
 	profit = None
 
-	for i in range(len(prices)-1,0,-1):
-		for j in range(i-1, 0, -1):
+	for i in range(0, len(prices)):
+		for j in range(i, len(prices)):
 			if i != j:
+				if profit == None or -prices[i] + prices[j] > profit:
+					profit = -prices[i] + prices[j]
+
+	# for i in range(len(prices)-1,0,-1):
+	# 	for j in range(i-1, 0, -1):
+	# 		if i != j:
 				#print(prices[i], prices[j], prices[i] - prices[j])
-				if profit == None or prices[i] - prices[j] > profit:
-					profit = prices[i] - prices[j]
-	# for i in range(len(prices)):	
+	# 			if profit == None or prices[i] - prices[j] > profit:
+	# 				profit = prices[i] - prices[j]
+	# # for i in range(len(prices)):	
 	# 	for j in range(len(prices)):
 	# 		# print(i, j, prices[j], prices[i], (prices[j]- prices[i]))
 	# 		if mr_profit == 0 or prices[j] - prices[i] > mr_profit:
@@ -27,11 +33,6 @@ def find_max_profit(prices):
 		# 	max = prices[i]
 		# print(min, max)
 	return profit
-
-print(find_max_profit([10, 7, 5, 8, 11, 9]))
-print(find_max_profit([100, 90, 80, 50, 20, 10]))
-print(find_max_profit([1050, 270, 1540, 3800, 2]))
-print(find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]))
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
